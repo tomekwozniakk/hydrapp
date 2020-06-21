fae6895326f3db97e1859f9d4448631ecb1ef13a
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-06-21 23:38:13 +0200

    added js

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,33 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const add = document.querySelector('.buttons__add--js');
+const remove = document.querySelector('.buttons__remove--js');
+const counter = document.querySelector('.container__counter--js');
+const key = new Date().toISOString().slice(0, 10);
+
+if(localStorage.getItem(key, counter.value)) {
+    counter.value = localStorage.getItem(key, counter.value);
+    counter.innerHTML = counter.value;
+}
+else {
+    counter.value = 0;
+    localStorage.setItem(key, counter.value);
+    counter.innerHTML = counter.value;
+}
+
+add.addEventListener('click', () => {
+    counter.value++;
+    counter.innerHTML = counter.value;
+    localStorage.setItem(key, counter.value);
+})
+
+remove.addEventListener('click', () => {
+    counter.value--;
+    if(counter.value < 0) {
+        counter.value = 0;
+    }
+    localStorage.setItem(key, counter.value);
+    counter.innerHTML = counter.value;
+})
+
