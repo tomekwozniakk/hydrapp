d7e20c6ff4132099bcf1c779d84869f51ac26b4f
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-06-22 20:56:32 +0200

    added animations

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,8 @@ registerSW();
 const add = document.querySelector('.buttons__add--js');
 const remove = document.querySelector('.buttons__remove--js');
 const counter = document.querySelector('.container__counter--js');
-const key = new Date().toISOString().slice(0, 10);
+const key = new Date().toLocaleString().slice(0, 10);
+const water = document.querySelector('.container__water');
 
 if(localStorage.getItem(key, counter.value)) {
     counter.value = localStorage.getItem(key, counter.value);
@@ -22,6 +23,10 @@ else {
 }
 
 add.addEventListener('click', () => {
+    if(counter.value == 0) {
+        water.classList.add('container__water--fill');
+        water.classList.remove('container__water--empty');
+    }
     counter.value++;
     counter.innerHTML = counter.value;
     localStorage.setItem(key, counter.value);
@@ -31,6 +36,10 @@ remove.addEventListener('click', () => {
     counter.value--;
     if(counter.value < 0) {
         counter.value = 0;
+    }
+    if(counter.value == 0) {
+        water.classList.remove('container__water--fill');
+        water.classList.add('container__water--empty')
     }
     localStorage.setItem(key, counter.value);
     counter.innerHTML = counter.value;
